e790862cfb16bf26d3b700000a4141aa643b706c
rashaxx9 <rasha200431@gmail.com>
2026-05-18 04:26:39 +0300

    Overwriting with local version

--- EpiWave_Chatbot/chatbot/api_chat.py
+++ EpiWave_Chatbot/chatbot/api_chat.py
@@ -24,5 +24,9 @@ def chat():
     
     return jsonify({'response': response_text})
 
+@app.route('/', methods=['GET'])
+def home():
+     return "EpiWave AI Server is running!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
